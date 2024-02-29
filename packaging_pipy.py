5d91fa2f6638f1837c28f9bf8601a77ac147c605
sunyeon-lee <odop@youhasoft.com>
2024-02-29 20:44:39 +0900

    Add pypi packaging script

---
+++ packaging_pipy.py
@@ -0,0 +1,18 @@
+import os, shutil
+
+def packaging_pipy():
+    # Copy files
+    shutil.rmtree('dist')
+
+    shutil.copytree('oauth_emailbackend/oauth_emailbackend', 
+                    'dist/src/oauth_emailbackend')
+    for f in ('LICENSE', 'README.md', 'pyproject.toml'):
+        if os.path.exists(f):
+            shutil.copyfile(f, os.path.join('dist', f))
+
+    # Build pypi package
+    os.chdir('dist')
+    os.system('python3 -m build')
+
+if __name__ == "__main__":
+    packaging_pipy()
